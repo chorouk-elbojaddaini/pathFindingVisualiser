402f2a6ae1cd050295e7ec65c41ccb1b237a3db5
chorouk-elbojaddaini <choroukelbojaddaini001@gmail.com>
2023-01-16 11:29:34 +0100

    DragDrop

--- pathFinding/src/app/app.module.ts
+++ pathFinding/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {DragDropModule} from '@angular/cdk/drag-drop';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CarreComponent } from './carre/carre.component';
