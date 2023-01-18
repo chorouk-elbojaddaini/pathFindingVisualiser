ccf47180c59b97f242424748a48e1575cbc5e479
rimha <hassaniidrissirim190@gmail.com>
2023-01-18 16:10:30 +0100

    condition

--- pathFinding/src/app/grille/grille.component.ts
+++ pathFinding/src/app/grille/grille.component.ts
@@ -5,87 +5,137 @@ import { Square } from '../shared/squareModele';
 @Component({
   selector: 'app-grille',
   templateUrl: './grille.component.html',
-  styleUrls: ['./grille.component.scss']
+  styleUrls: ['./grille.component.scss'],
 })
 export class GrilleComponent {
-  numberSquares:number;
-  board:Board;
-  nodes:Square[][];
-  
+  numberSquares: number;
+  board: Board;
+  nodes: Square[][];
+
   ngOnInit(): void {
-   
     let wi = window.innerWidth;
-    this.numberSquares = Math.trunc(wi/20);
-    this.board  = new Board(window.innerWidth,this.numberSquares*3,null,null,false,false,false,false);
+    this.numberSquares = Math.trunc(wi / 20);
+    this.board = new Board(
+      window.innerWidth,
+      this.numberSquares * 3,
+      null,
+      null,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false
+    );
     this.initialiseGrid();
-    
+
     console.log(this.nodes);
-    console.log("hi");
+    console.log('hi');
     console.log(this.numberSquares);
   }
-  ngOnchange(){
-
-  }
-  test(node:Square){
+  ngOnchange() {}
+  test(node: Square) {
     console.log(` this is i=>${node.row} ,this is j =>${node.col}`);
   }
-  test2(){
-    console.log("click events");
+  test2() {
+    console.log('click events');
   }
 
-  initialiseGrid(){
+  initialiseGrid() {
     this.nodes = [];
-    for(let i=0;i<window.innerHeight/30;i++){
+    for (let i = 0; i < window.innerHeight / 30; i++) {
       this.nodes[i] = [];
-      for(let j=0;j<Math.trunc(window.innerWidth/20);j++){
-        this.nodes[i][j] = new Square(i,j,false,false,false,false);
-        if(i==8 && j== Math.trunc(this.numberSquares/2)-15){
-          this.nodes[i][j].isStartingbox=true;
+      for (let j = 0; j < Math.trunc(window.innerWidth / 20); j++) {
+        this.nodes[i][j] = new Square(i, j, false, false, false, false);
+        if (i == 8 && j == Math.trunc(this.numberSquares / 2) - 15) {
+          this.nodes[i][j].isStartingbox = true;
         }
-        if(i==8&& j== Math.trunc(this.numberSquares/2)+10){
-          this.nodes[i][j].isTargetBox=true;
+        if (i == 8 && j == Math.trunc(this.numberSquares / 2) + 10) {
+          this.nodes[i][j].isTargetBox = true;
         }
       }
     }
   }
 
-  statusNode(node:Square):string{
-    if(node.isStartingbox){
-      return "start";
+  statusNode(node: Square): string {
+    if (node.isStartingbox) {
+      return 'start';
     }
-    if(node.isTargetBox){
-      return "target";
+    if (node.isTargetBox) {
+      return 'target';
     }
-    return "normal";
+    return 'normal';
   }
-  eventsListener(){
+  eventsListener() {
     // for(let i=0;i<window.innerHeight/30;i++){
     //   for(let j=0;j<this.numberSquares-1;j++){
     //     this.nodes[i][j].
     //   }
     // }
   }
-  mouseDown(node:Square){
-    this.board.mouseDown = true;
-    this.board.currentNode=node;
-    console.log(`this is the MOUSE DOWN ${node.row} ${node.col}`);
-  }
-  mouseEnter(node:Square){
-    if(this.board.mouseDown){
-      this.board.mouseEnter  = true;
-      this.board.enteredNode = node;
-      console.log(`this is the MOUSE ENTER ${node.row} ${node.col}`);
+  mouseDown(node: Square) {
+    if (node.isStartingbox ) {
+      this.board.mouseDown = true;
+      this.board.currentNode = node;
+      this.board.isSelectedNodeStart=true;
+      console.log(`this is the MOUSE DOWN ${node.row} ${node.col}`);
+    }
+    if(node.isTargetBox){
+      this.board.mouseDown = true;
+      this.board.currentNode = node;
+      this.board.isSelectedNodeEnd=true;
     }
   }
-  mouseUp(node:Square){
-    if(this.board.mouseEnter){
-      this.board.mouseUp = true;
-      this.board.mouseEnter=false;
-      this.board.mouseDown=false;
+  mouseLeave(node: Square) {
+    if (this.board.mouseDown && this.board.isSelectedNodeStart) {
+      node.isStartingbox = false;
+      console.log(`this is the MOUSE LEAVE ${node.row} ${node.col}`);
+    }
+    if(this.board.mouseDown && this.board.isSelectedNodeEnd){
+      node.isTargetBox=false;
+    }
+  }
+  mouseEnter(node: Square) {
+    if (this.board.mouseDown && this.board.isSelectedNodeStart) {
+      this.board.mouseEnter = true;
       this.board.enteredNode = node;
-      let element = document.getElementById(`${this.board.enteredNode.row}-${this.board.enteredNode.col}`).parentElement
-      console.log(element);
-      element.style.backgroundColor ='red';
+      if(!node.isTargetBox){
+        node.isStartingbox = true;
+      }
+      
+
+      console.log(`this is the MOUSE ENTER ${node.row} ${node.col}`);
+    }
+    if(this.board.mouseDown && this.board.isSelectedNodeEnd){
+      this.board.mouseEnter = true;
+      this.board.enteredNode = node;
+      if(!node.isStartingbox){
+        node.isTargetBox = true;
+      }
+     
+    }
+  }
+  mouseUp(node: Square) {
+    if (this.board.mouseEnter && this.board.isSelectedNodeStart && !node.isTargetBox) {
+      this.board.mouseUp = true;
+      this.board.mouseEnter = false;
+      this.board.mouseDown = false;
+      this.board.enteredNode = node;
+      if(!node.isTargetBox){
+        node.isStartingbox = true;
+      }
+
+      console.log(`this is the MOUSE UP ${node.row} ${node.col}`);
+    }
+    if(this.board.mouseEnter && this.board.isSelectedNodeEnd &&!node.isStartingbox){
+      this.board.mouseUp = true;
+      this.board.mouseEnter = false;
+      this.board.mouseDown = false;
+      this.board.enteredNode = node;
+      if(!node.isStartingbox){
+        node.isTargetBox = true;
+      }
+
       console.log(`this is the MOUSE UP ${node.row} ${node.col}`);
     }
   }
