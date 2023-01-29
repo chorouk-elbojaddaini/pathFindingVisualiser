3b73d14e960031aedbc195a1c3c36929e28cf150
chorouk-elbojaddaini <choroukelbojaddaini001@gmail.com>
2023-01-29 13:25:34 +0100

    visualiseBtn

--- pathFinding/src/app/navbar/navbar.component.ts
+++ pathFinding/src/app/navbar/navbar.component.ts
@@ -11,7 +11,7 @@ export class NavbarComponent implements OnInit {
   selectedMaze:string ;
   typeAlgorithm: FormGroup;
   typeMazePattern:FormGroup;
-  
+  valueVisualise:string = 'Visualise';
 
   constructor(
     private fb: FormBuilder,
@@ -23,12 +23,18 @@ export class NavbarComponent implements OnInit {
   ngOnInit(): void {
     this.initAlgoForm();
     this.initMazePattern();
-
+    
   }
   ngDoCheck() {
-    this.chosenAlgoName();
     this.chosenMaze();
+    if(this.dijkstraService.chosenAlgo != this.valueVisualise){
+      this.chosenAlgoName();
+    let btnVisualise = document.getElementById("button");
+      this.valueVisualise = this.dijkstraService.chosenAlgo;
+      btnVisualise.innerHTML = 'visualise'+ this.dijkstraService.chosenAlgo;
 
+    }
+    
   }
   initAlgoForm() {
     this.typeAlgorithm = this.fb.group({
@@ -42,12 +48,14 @@ export class NavbarComponent implements OnInit {
   }
 
   choseAlgorithme() {
+    let btnVisualise = document.getElementById("button");
     switch (this.dijkstraService.chosenAlgo) {
       case 'A* Search':
         this.dijkstraService.algo = 'astar';
         break;
       case "Dijkstra's Algorithm":
         this.dijkstraService.algo = 'dijkstra';
+
         break;
       case "Greedy Best-first Search":
         this.dijkstraService.algo = 'greedy'
@@ -61,6 +69,8 @@ export class NavbarComponent implements OnInit {
        case "Depth-first Search":
         this.dijkstraService.algo = 'depth'
         break;
+        default:  
+      btnVisualise.innerHTML = 'pick an algorithm';
     }
   }
   addPerson() {
@@ -70,6 +80,8 @@ export class NavbarComponent implements OnInit {
   }
   chosenAlgoName() {
     this.dijkstraService.chosenAlgo = this.typeAlgorithm.value.algo;
+    console.log('algochosen',this.dijkstraService.chosenAlgo);
+  
   }
   chosenMaze(){
    
@@ -84,5 +96,13 @@ export class NavbarComponent implements OnInit {
   }
   clearPath(){
     this.dijkstraService.clearPath = true;
+  }
+  visualisebtn(){
+    let btnVisualise = document.getElementById("button");
+    console.log("visualise btn");
+     
+      btnVisualise.innerHTML = 'pick an algorithm';
+
+     
   }
 }
